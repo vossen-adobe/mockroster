5c253420721948f836d4a60c30f7ca8eccc149f8
jansseneda-adobe <danimaejanssen@gmail.com>
2019-05-22 22:14:11 -0500

    round 2: fight!

---
+++ src/main/java/com/dm/mockroster/MockrosterApplication.java
@@ -0,0 +1,13 @@
+package com.dm.mockroster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockrosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockrosterApplication.class, args);
+	}
+
+}
