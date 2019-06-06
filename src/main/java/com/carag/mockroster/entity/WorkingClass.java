a03d898cffdd1bb3626e1624d4dfd95ac5a962c4
jansseneda-adobe <danimaejanssen@gmail.com>
2019-06-06 00:00:20 -0500

    simple logging log4j

--- src/main/java/com/carag/mockroster/entity/WorkingClass.java
+++ src/main/java/com/carag/mockroster/entity/WorkingClass.java
@@ -1,16 +1,17 @@
 package com.carag.mockroster.entity;
 
 import com.carag.mockroster.data.repository.UserRepository;
-import com.carag.mockroster.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import javax.annotation.PostConstruct;
-import java.io.File;
 import java.io.IOException;
 
 @Component
 public class WorkingClass {
+
+    private final static Logger logger = LogManager.getLogger(WorkingClass.class);
 
     @Autowired
     private UserRepository userRepository;
@@ -19,7 +20,7 @@ public class WorkingClass {
     void execute() throws IOException {
 
 
-
+       // logger.info("ARRRRRR");
 
 
         User u = new User();
