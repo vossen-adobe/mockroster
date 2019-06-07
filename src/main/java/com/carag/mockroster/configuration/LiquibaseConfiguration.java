3d2c09bfc55b779e5dbecfaf71321324c144feff
jansseneda-adobe <danimaejanssen@gmail.com>
2019-06-06 22:44:05 -0500

    works

--- src/main/java/com/carag/mockroster/configuration/LiquibaseConfiguration.java
+++ src/main/java/com/carag/mockroster/configuration/LiquibaseConfiguration.java
@@ -1,16 +1,16 @@
 package com.carag.mockroster.configuration;
 
-import com.sun.javafx.binding.Logging;
 import liquibase.Liquibase;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
-import java.io.StringWriter;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -23,7 +23,8 @@ public class LiquibaseConfiguration {
     @PostConstruct
     public void liquibase() throws SQLException, LiquibaseException {
 
-
+        Logger logger = LoggerFactory.getLogger("abc");
+        logger.warn("AWEDAWADAFAG");
         String url = env.getProperty("spring.datasource.url");
         String username = env.getProperty("spring.datasource.username");
         String password = env.getProperty("spring.datasource.password");
--- src/main/java/com/carag/mockroster/entity/WorkingClass.java
+++ src/main/java/com/carag/mockroster/entity/WorkingClass.java
@@ -3,15 +3,13 @@ package com.carag.mockroster.entity;
 import com.carag.mockroster.data.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 
 @Component
 public class WorkingClass {
 
-    private final static Logger logger = LogManager.getLogger(WorkingClass.class);
 
     @Autowired
     private UserRepository userRepository;
