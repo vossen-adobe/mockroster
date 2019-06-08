16696958875c8778a74b8e86a211707108c8c3e8
jansseneda-adobe <danimaejanssen@gmail.com>
2019-06-07 19:34:23 -0500

    it works, now destroy it in favor of just setting liquibase log

--- src/main/java/com/carag/mockroster/configuration/LiquibaseConfiguration.java
+++ src/main/java/com/carag/mockroster/configuration/LiquibaseConfiguration.java
@@ -4,10 +4,8 @@ import liquibase.Liquibase;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
-import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configurator;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
@@ -26,27 +24,17 @@ public class LiquibaseConfiguration {
     @PostConstruct
     public void liquibase() throws SQLException, LiquibaseException {
 
+        Logger logger = LogManager.getLogger(this.getClass());
         String url = env.getProperty("spring.datasource.url");
         String username = env.getProperty("spring.datasource.username");
         String password = env.getProperty("spring.datasource.password");
         String changeLog = env.getProperty("spring.liquibase.change-log").replace("classpath:", "");
 
-        Logger logger = LogManager.getLogger(this.getClass());
-        Level level = logger.getLevel();
+        logger.info("Beginning CSV load... ");
+        new Liquibase(changeLog, new ClassLoaderResourceAccessor(),
+                new JdbcConnection(DriverManager.getConnection(url, username, password))).update("");
+        logger.info("Finished CSV load... ");
 
-        try {
-            logger.info("Beginning CSV load... ");
-            setLevel(Level.WARN);
-            new Liquibase(changeLog, new ClassLoaderResourceAccessor(),
-                    new JdbcConnection(DriverManager.getConnection(url, username, password))).update("");
-        } finally {
-            setLevel(level);
-            logger.info("Finished CSV load... ");
-        }
     }
 
-    private void setLevel(Level level){
-        if (Boolean.parseBoolean(env.getProperty("spring.liquibase.silentSQL")))
-            Configurator.setLevel(LogManager.getRootLogger().getName(), level);
-    }
 }
